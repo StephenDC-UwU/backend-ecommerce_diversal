79e38f8c044237e465249a90c4cb462d8e558fd3
Filimisco <82726794+StephenDC-UwU@users.noreply.github.com>
2025-10-24 19:48:03 +0200

    changes_on_server

--- config/server.ts
+++ config/server.ts
@@ -3,7 +3,7 @@ import { url } from "inspector";
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 5000),
-  url: env('SRVER_URL'),
+  url: env('SERVER_URL'),
   proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
