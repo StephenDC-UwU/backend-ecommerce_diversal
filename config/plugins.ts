d06fd96dd59eb521dc88f4f7fe795c0c82a83db1
Filimisco <82726794+StephenDC-UwU@users.noreply.github.com>
2025-12-20 10:23:03 +0100

    add external service cloudnary

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,22 @@
-export default () => ({
+import { config } from "process";
+
+export default ({env}) => ({
     'content-releases': {
     enabled: true,
   },
+  upload: {
+    config:{
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET')
+      },
+      actionOptions:{
+        upload:{},
+        uploadStream: {},
+        delete: {},
+      }
+    }
+  }
 });
